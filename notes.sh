abcd3c21380e6fbb717132ac38d95073c572f079
William Sanders <william.s.sanders@gmail.com>
2021-02-22 13:24:50 -0500

    Added .gitignore, updated download links

--- notes.sh
+++ notes.sh
@@ -3,7 +3,8 @@
 # write test!
 
 # Setup GeneMark ES/ET/EP Key
-wget http://topaz.gatech.edu/GeneMark/tmp/GMtool_W1C3F/gm_key_64.gz
+#wget http://topaz.gatech.edu/GeneMark/tmp/GMtool_W1C3F/gm_key_64.gz
+wget http://topaz.gatech.edu/GeneMark/tmp/GMtool_UfdZ7/gm_key_64.gz
 gunzip gm_key_64.gz
 cp gm_key_64 ~/.gm_key
 cp gm_key_64 ~/.gm_key_64
