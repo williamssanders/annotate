dad1943c1ef81d3fe98d08e499465cc1e79b5f68
William Sanders <william.s.sanders@gmail.com>
2021-04-06 16:39:58 -0400

    Trimmomatic command

--- notes.sh
+++ notes.sh
@@ -16,3 +16,5 @@ cp gm_key_64 ~/.gm_key_64
 
 # need to configure repeat masker, repeat modeler, and recon, using an overlay
 # using the same overlay for all seems to work
+
+java -jar /opt/Trimmomatic/trimmomatic-0.39.jar PE /projects/ssander/rattles/SRR628245_1.fastq /projects/ssander/rattles/SRR628245_2.fastq /projects/ssander/rattles/rattles_rnaseq_pe_1.fq.gz /projects/ssander/rattles/rattles_rnaseq_pe_2.fq.gz /projects/ssander/rattles/rattles_rnaseq_se_1.fq.gz /projects/ssander/rattles/rattles_rnaseq_se_2.fq.gz ILLUMINACLIP:/opt/Trimmomatic-0.39/adapters/all_adapters.fa:2:30:10:2:keepBothReads LEADING:3 TRAILING:3 MINLEN:36
